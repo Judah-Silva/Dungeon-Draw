7929187a08c190ea3b05475da2cdb15bc429e19b
Erik <ermorris@csumb.edu>
2024-04-17 15:51:31 -0500

    Card Fix
    Accidentally removed a function in card but its back now

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -79,4 +79,18 @@ public class Card : MonoBehaviour
 			        }
 		        }
 	        }
+	        public static void PlayCard(GameObject card)
+                {
+                    Discard.currentSize++;
+                    Hand.currentSize--;
+                    
+                    // do card effect
+                    
+                    Destroy(card);
+            
+                    if (Hand.currentSize == 0)
+                    {
+                        Draw.DrawFromPile();
+                    }
+                }
 }
