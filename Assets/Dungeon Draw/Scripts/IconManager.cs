5d83b23c1e9734e022064976167b4267069126e4
mbelles21 <mbelles@csumb.edu>
2024-04-22 19:53:04 -0700

    icon generation + map advancement

--- Assets/Dungeon Draw/Scripts/IconManager.cs
+++ Assets/Dungeon Draw/Scripts/IconManager.cs
@@ -9,28 +9,78 @@ public class IconManager : MonoBehaviour
     public string shopScene = "ShopScene";
     public string restScene = "RestSpotScene";
     public string eventScene = "EventScene";
+
+    public MapManager mapManager;
+
+    private bool wasClicked = false;
     
     private void OnMouseDown()
     {
+        if (wasClicked)
+        {
+            Debug.Log("already visited");
+            return;
+        }
+        
         if (gameObject.CompareTag("battle"))
         {
-            SceneManager.LoadScene(battleScene);
+            Debug.Log("battle engaged");
+            mapManager.SpawnIcons(gameObject.transform.position);
+            // SceneManager.LoadScene(battleScene);
         }
         else if (gameObject.CompareTag("shop"))
         {
-            SceneManager.LoadScene(shopScene);
+            Debug.Log("shop entered");
+            mapManager.SpawnIcons(gameObject.transform.position);
+            // SceneManager.LoadScene(shopScene);
         }
         else if (gameObject.CompareTag("rest"))
         {
-            SceneManager.LoadScene(restScene);
+            Debug.Log("resting");
+            mapManager.SpawnIcons(gameObject.transform.position);
+            // SceneManager.LoadScene(restScene);
         }
         else if (gameObject.CompareTag("event"))
         {
-            SceneManager.LoadScene(eventScene);
+            Debug.Log("event triggered");
+            mapManager.SpawnIcons(gameObject.transform.position);
+            // SceneManager.LoadScene(eventScene);
         }
         else
         {
             Debug.Log("this icon doesn't exist");
+        }
+
+        DisableIcons();
+    }
+
+    private void DisableIcons()
+    {
+        wasClicked = true;
+        float checkDist = 5f;
+
+        // check for icons above
+        Ray rayUp = new Ray(transform.position, transform.forward);
+        RaycastHit hitUp;
+        if (Physics.Raycast(rayUp, out hitUp, checkDist))
+        {
+            IconManager hitIcon = hitUp.collider.GetComponent<IconManager>();
+            if (hitIcon != null && !hitIcon.wasClicked)
+            {
+                hitIcon.DisableIcons();
+            }
+        }
+
+        // check for icons below
+        Ray rayDown = new Ray(transform.position, transform.forward * -1f);
+        RaycastHit hitDown;
+        if(Physics.Raycast(rayDown, out hitDown, checkDist))
+        {
+            IconManager hitIcon = hitDown.collider.GetComponent<IconManager>();
+            if (hitIcon != null && !hitIcon.wasClicked)
+            {
+                hitIcon.DisableIcons();
+            }
         }
     }
 }
--- Assets/Dungeon Draw/Scripts/MapManager.cs
+++ Assets/Dungeon Draw/Scripts/MapManager.cs
@@ -1,11 +1,80 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Random = UnityEngine.Random;
 
 public class MapManager : MonoBehaviour
 {
     public string mainMenuScene = "MainMenu";
+
+    public Transform startPoint;
+    public GameObject battleIcon;
+    public GameObject shopIcon;
+    public GameObject restIcon;
+    public GameObject eventIcon;
+    
+    public static float iconSpacing = 5f;
+
+    private void Start()
+    {
+        Vector3 startPos = startPoint.position;
+        SpawnIcons(startPos);
+    }
+
+    public void SpawnIcons(Vector3 startPos)
+    {
+        float columnSpacing = 5f;
+        for (int i = 0; i < 3; i++)
+        {
+            if (i == 0)
+            {
+                columnSpacing = 4f;
+            }
+            if (i == 1)
+            {
+                columnSpacing = -1f;
+            }
+            if (i == 2)
+            {
+                columnSpacing = -6f;
+            }
+            
+            Vector3 spawnPos = new Vector3(startPos.x + iconSpacing, startPos.y, columnSpacing);
+            int r = Random.Range(1, 6);
+
+            if (r == 1)
+            {
+                GameObject newIcon = Instantiate(battleIcon, spawnPos, Quaternion.identity);
+            }
+            else if (r == 2)
+            {
+                GameObject newIcon = Instantiate(shopIcon, spawnPos, Quaternion.identity);
+            }
+            else if (r == 3)
+            {
+                GameObject newIcon = Instantiate(restIcon, spawnPos, Quaternion.identity);
+            }
+            else if (r == 4)
+            {
+                GameObject newIcon = Instantiate(eventIcon, spawnPos, Quaternion.identity);
+            }
+            else if (r == 5)
+            {
+                GameObject newIcon = Instantiate(battleIcon, spawnPos, Quaternion.identity);
+            }
+            else if (r == 6)
+            {
+                GameObject newIcon = Instantiate(battleIcon, spawnPos, Quaternion.identity);
+            }
+
+            // columnSpacing -= 5f;
+        }
+        
+        
+        
+    }
 
     public void ReturnToMainMenu()
     {
