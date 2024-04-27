3a3540eee7bd9368f5b75663ee5b9fd592093875
Erik <ermorris@csumb.edu>
2024-04-26 17:33:43 -0700

    Shop and Event essentially complete
    Just need to have access to the card database and I need to know how the currency will work. Then just some small tweaks and they should be ready.

--- Assets/Dungeon Draw/Scripts/Shop.cs
+++ Assets/Dungeon Draw/Scripts/Shop.cs
@@ -6,31 +6,53 @@ using UnityEngine.SceneManagement;
 using System.IO;
 using System.Numerics;
 using TMPro;
+//using UnityEngine.UIElements;
 using Random = UnityEngine.Random;
+using UnityEngine.UI;
 
 [System.Serializable]
 public class shopItem
 {
+    public void setCard(List<int> card)
+    {
+        //ac = new ActualCard(card);
+        fillSlot();
+    }
+
+    public void fillSlot()
+    {
+        Slot.GetComponentInChildren<TMP_Text>().text = price.ToString();
+        
+    }
     public int price;
     public int ID;
     public Texture image;
     public GameObject Slot;
+    //public ActualCard ac = null;
 }
 public class Shop : MonoBehaviour
 {
     public GameObject card;
 
+    //public GameObject gameManager;
+    //private CardDataBase = cardDB;
+    //private RelicDataBase = relicDB;
+
     public int cardSlotAmount = 5;
-    public List<GameObject> cardSlots;
-    public List<GameObject> relicSlots;
-    public shopItem[] cardShopItems = new shopItem[5];
+    public List<GameObject> cardSlots; //Takes gameobjects from scene for card slots and booster slots
+    public List<GameObject> relicSlots; //Same but for relic slots
+    public shopItem[] cardShopItems = new shopItem[5]; //Hold the actual cards
     public shopItem[] boosterPacks = new shopItem[2];
     public shopItem[] relics = new shopItem[6];
+
+    public GameObject flashPanel;
+    public Image img;
+    public AnimationCurve curve;
 
     public string MapSceneName = "Map";
 
     public string shopFileName = "shop";
-    public List<string> shopRows = new List<string>();
+    private List<string> shopRows = new List<string>();
     private int cMax, rMax,  bMax; //Values to know where in the stack cards/relics/boosterpacks end || Set in the loadShopItems function
     void Start()
     {
@@ -43,11 +65,124 @@ public class Shop : MonoBehaviour
         //alternately, can just set the sprite of the cardSlot rather than placing a prefab. Not sure what will work better yet
 
 
-        loadShopItems(); // Loads shopRows stack from shop.txt inside the resource folder
-        // Now randomly selects cards/relics/boosterpacks
-        // Debug.Log("Card Max - " + cMax);
-        // Debug.Log("Boost Max - " + bMax);
-        // Debug.Log("Relic Max - " + rMax);
+        loadShopItemsFromText(); // Loads shopRows stack from shop.txt inside the resource folder and then populate shop
+        
+        //Preparation for CardDataBase
+        /*gameManager = GameObject.Find("GameManager");
+         cardDB = gameManager.GetComponent<CardDataBase>();
+         int ran = 0;
+         for (int i = 0; i<cardShopItems.Length; i++)
+         {
+            cardShopItems[i].Slot = cardSlots[i];
+            ran = Random.Range(0, cardDB.allCards.Count);
+            cardShopItems[i].setCard(cardDB.getCard(ran));
+         }
+         
+         */
+    }
+
+    public void CardPressed(int buttin)
+    {
+        Debug.Log("Card "+ buttin +" Pressed");
+        //Check for money
+        bool money = false;
+        Debug.Log(cardShopItems[buttin].price);
+        if (money) // if enough money
+        {
+             //money -= cardShopItems[buttin].price;
+             //cardDB.heldCards.add(cardDB.getCard(cardShopItems[buttin].ac.cardID))
+             repopulateShopItem(buttin);
+        }
+        else
+        {
+            StartCoroutine(declineFade());
+        }
+    }
+
+    public void BoosterPressed(int buttin)
+    {
+        Debug.Log("Booster "+ buttin +" Pressed");
+        bool money = false;
+        Debug.Log(boosterPacks[buttin].price);
+        if (money) // if enough money
+        {
+            
+        }
+        else
+        {
+            StartCoroutine(declineFade());
+        }
+    }
+
+    public void RelicPressed(int buttin)
+    {
+        Debug.Log("Relic "+ buttin +" Pressed");
+        bool money = false;
+        Debug.Log(relics[buttin].price);
+        if (money) // if enough money
+        {
+            
+        }
+        else
+        {
+            StartCoroutine(declineFade());
+        }
+    }
+
+    public void RemoveCard()
+    {
+        Debug.Log("Remove Card Pressed");
+    }
+
+    public void leaveScene()
+    {
+        SceneManager.LoadScene(MapSceneName);
+    }
+
+    private void repopulateShopItem(int shopId)
+    {
+        // int ran = 0;
+        // ran = Random.Range(0, cardDB.allCards.Count);
+        // cardShopItems[shopId].setCard(cardDB.getCard(ran));
+         
+    }
+    
+
+    IEnumerator declineFade()
+    {
+        float t = .85f;
+        while (t > 0f)
+        {
+            t -= Time.deltaTime;
+            float a = curve.Evaluate(t);
+            img.color = new Color(1f, 0f, 0f, a);
+            yield return 0;
+        }
+    }
+
+    private void loadShopItemsFromText()
+    {
+        string shopFile = $"{Application.dataPath}{"/Dungeon Draw/Resources/"}{shopFileName}.txt";
+        Debug.Log($"Loading shop file: {shopFile}");
+
+  
+        using (StreamReader sr = new StreamReader(shopFile))
+        {
+            string line = "";
+            while ((line = sr.ReadLine()) != null)
+            {
+                shopRows.Add(line);
+                //Debug.Log("Line pushed - " + line);
+                if (line == "[RELICS]")
+                    bMax = shopRows.Count - 1;
+                if (line == "[BOOSTER PACKS]")
+                    cMax = shopRows.Count - 1;
+            }
+
+            rMax = shopRows.Count - 1;
+
+            sr.Close();
+        }
         int ran, priceMinIndex, priceMaxIndex, priceMin, priceMax;
         for (int i = 0; i < 13; i++)
         {
@@ -96,57 +231,6 @@ public class Shop : MonoBehaviour
                 relics[i%7].Slot.GetComponentInChildren<TMP_Text>().text = relics[i%7].price.ToString();
             }
         }
-    }
-
-    public void CardPressed()
-    {
-        Debug.Log("Card Pressed");
-    }
-
-    public void BoosterPressed()
-    {
-        Debug.Log("Booster Pressed");
-    }
-
-    public void RelicPressed()
-    {
-        Debug.Log("Relic Pressed");
-    }
-
-    public void RemoveCard()
-    {
-        Debug.Log("Remove Card Pressed");
-    }
-
-    public void leaveScene()
-    {
-        SceneManager.LoadScene(MapSceneName);
-    }
-
-    private void loadShopItems()
-    {
-        string shopFile = $"{Application.dataPath}{"/Dungeon Draw/Resources/"}{shopFileName}.txt";
-        Debug.Log($"Loading shop file: {shopFile}");
-
-  
-        using (StreamReader sr = new StreamReader(shopFile))
-        {
-            string line = "";
-            while ((line = sr.ReadLine()) != null)
-            {
-                shopRows.Add(line);
-                //Debug.Log("Line pushed - " + line);
-                if (line == "[RELICS]")
-                    bMax = shopRows.Count - 1;
-                if (line == "[BOOSTER PACKS]")
-                    cMax = shopRows.Count - 1;
-            }
-
-            rMax = shopRows.Count - 1;
-
-            sr.Close();
-        }
-        // Debug.Log(shopRows);
     }
 
 }
